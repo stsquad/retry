797b2309e3d1a8a79fb0cc35be2743680a4e56d1
Alex Bennée <ajb@cbnl.com>
2013-02-28 17:39:08 +0000

    retry: try using pty's instead of pipes so things like scp don't get all coy
    The indentation also seemed to be screwed up. Should all be good now.

--- retry.py
+++ retry.py
@@ -13,12 +13,12 @@
 #
 
 from argparse import ArgumentParser
-from os import kill, getenv, O_NONBLOCK
+from os import fdopen, kill, getenv, O_NONBLOCK
 from subprocess import Popen, STDOUT, PIPE
 from fcntl import fcntl, F_SETFL
-import shlex
 import signal
 import sys
+import pty
 
 #
 # Command line options
@@ -35,59 +35,60 @@ global p
 global complete
 
 def signal_handler(sig, frame):
-	global p
-	global complete
-        print 'You caught %d Ctrl+C! Killing %d' % (sig, p.pid)
-	print "state of process is: %s" % (p.poll())
-	complete=True
-	p.terminate()
+    global p
+    global complete
+    print 'You caught %d Ctrl+C! Killing %d' % (sig, p.pid)
+    print "state of process is: %s" % (p.poll())
+    complete=True
+    p.terminate()
 
 # Start of code
 if __name__ == "__main__":
-	global complete
-	args = parser.parse_args()
-
-        if args.verbose: print "command is %s" % (args.command)
-        if args.invert and args.limit==None:
-                print "You must define a limit if you have inverted the return code test"
-                exit(-1)
-
-	# Trap SIGTERM, SIGINT nicely
-	signal.signal(signal.SIGINT, signal_handler)
-
-	complete=False
-        run_count=0
-	while not complete:
-                cmd = " ".join(args.command)
-                print "cmd is: %s" % (cmd)
-		p = Popen(args.command, shell=False, bufsize=1, stdout=PIPE, stderr=STDOUT)
-		if args.verbose > 1: print "Running: pid:%d, rc:%s" % (p.pid, p.returncode)
-		fcntl(p.stdout.fileno(), F_SETFL, O_NONBLOCK)
-                return_code = 0
-		running = True
-		while running:
-			try:
-				out = p.stdout.read()
-				if len(out)>0: sys.stdout.write(out)
-			except IOError:
-				pass
-			except:
-				print "another exceptions"
-
-			x = p.poll()
-			if x != None:
-				print "child finished: x=%s rc = %d" % (x, p.returncode)
-				running = False
-                                return_code = p.returncode
-
-                run_count = run_count + 1
-
-                # Process our exit conditions
-                if args.test == True: complete = True
-                if run_count >= args.limit: complete = True
-                if args.invert:
-                        if return_code != 0: complete = True
-                else:
-                        if return_code == 0: complete = True
-
-        print "Ran command %d times" % (run_count)
+    global complete
+    args = parser.parse_args()
+    
+    if args.verbose: print "command is %s" % (args.command)
+    if args.invert and args.limit==None:
+        print "You must define a limit if you have inverted the return code test"
+        exit(-1)
+            
+    # Trap SIGTERM, SIGINT nicely
+    signal.signal(signal.SIGINT, signal_handler)
+            
+    complete=False
+    run_count=0
+    while not complete:
+        cmd = " ".join(args.command)
+        # we need a pty if we don't want children assuming they are not interactive
+        master, slave = pty.openpty()
+        p = Popen(cmd, shell=True, bufsize=1, stdout=slave, stderr=slave, close_fds=True)
+        stdout = fdopen(master)
+        if args.verbose > 1: print "Running: pid:%d, rc:%s" % (p.pid, p.returncode)
+        return_code = 0
+        running = True
+        while running:
+            try:
+                out = stdout.read()
+                if len(out)>0: sys.stdout.write(out)
+            except IOError:
+                pass
+            except:
+                print "another exceptions"
+                                
+            x = p.poll()
+            if x != None:
+                print "child finished: x=%s rc = %d" % (x, p.returncode)
+                running = False
+                return_code = p.returncode
+                                    
+        run_count = run_count + 1
+                                    
+        # Process our exit conditions
+        if args.test == True: complete = True
+        if run_count >= args.limit: complete = True
+        if args.invert:
+            if return_code != 0: complete = True
+        else:
+            if return_code == 0: complete = True
+                                                        
+    print "Ran command %d times" % (run_count)
