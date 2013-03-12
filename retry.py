fe1dcf270c2b5f8e269db534969894adbfea534a
Alex Bennée <alex@bennee.com>
2013-03-12 20:56:21 +0000

    retry.py: bypass a lot of unneeded hairy code by just passing the tty down. From the commentary on the SO question (http://stackoverflow.com/questions/15200700/how-do-i-set-the-terminal-foreground-process-group-for-a-process-im-running-und/15257702?noredirect=1#):
    "The setpgrp() call creates a new process group in the same session,
    so that the new process will receive any ctrl-c/ctrl-z/etc from the
    user, and your retry script won't. Then the tcsetpgrp() makes the new
    process group be the foreground one on the controlling tty. The new
    process gets a SIGTTOU when that happens (because since the setpgrp(),
    it has been in a background process group), which normally would make
    the process stop, so that's the reason for ignoring SIGTTOU. We set
    the SIGTTOU handler back to whatever it was before, to minimize the
    chance of the subprocess being confused by an unexpected signal table.
    Since the subprocess is now in the foreground group for the tty, its
    tcgetpgrp() and getpgrp() will be the same, and isatty(1) will be true
    (assuming the stdout it inherits from retry.py actually is a tty). You
    don't need to proxy traffic between the subprocess and the tty, which
    lets you ditch all the select event handling and
    fcntl-nonblocking-setting."
    
    Thanks to "thepaul" :-)

--- retry.py
+++ retry.py
@@ -13,13 +13,10 @@
 #
 
 from argparse import ArgumentParser
-from subprocess import Popen, STDOUT, PIPE
-from fcntl import fcntl, F_SETFL
-import signal
 import os
-import sys
-import pty
-import select
+import signal
+import subprocess
+import itertools
 
 #
 # Command line options
@@ -35,75 +32,26 @@ parser.add_argument('command', nargs='*', help="The command to run. You can prec
 global p
 global complete
 
-def signal_handler(sig, frame):
-    global p
-    global complete
-    print 'You caught %d Ctrl+C! Killing %d' % (sig, p.pid)
-    print "state of process is: %s" % (p.poll())
-    complete=True
-    p.terminate()
+def become_tty_fg():
+    os.setpgrp()
+    hdlr = signal.signal(signal.SIGTTOU, signal.SIG_IGN)
+    tty = os.open('/dev/tty', os.O_RDWR)
+    os.tcsetpgrp(tty, os.getpgrp())
+    signal.signal(signal.SIGTTOU, hdlr)
 
-# Start of code
 if __name__ == "__main__":
-    global complete
     args = parser.parse_args()
 
     if args.verbose: print "command is %s" % (args.command)
     if args.invert and args.limit==None:
-        print "You must define a limit if you have inverted the return code test"
-        exit(-1)
+        sys.exit("You must define a limit if you have inverted the return code test")
 
-    # Find out our foreground process group
-    our_tty = os.open("/dev/tty", os.O_RDONLY)
-    our_tty_pgrp = os.tcgetpgrp(our_tty)
-    if args.verbose > 1: print "terminal process group = %d" % (our_tty_pgrp)
-
-    # Trap SIGTERM, SIGINT nicely
-    signal.signal(signal.SIGINT, signal_handler)
-
-    complete=False
-    run_count=0
-    while not complete:
-        # flags
-        return_code = 0
-        running = True
-
-        # we need a pty if we don't want children assuming they are not interactive
-        master, slave = pty.openpty()
-        m_pty = os.fdopen(master)
-        fcntl(m_pty.fileno(), F_SETFL, os.O_NONBLOCK)
-        io_poll = select.poll()
-        io_poll.register(m_pty, (select.POLLIN|select.POLLPRI|select.POLLERR|select.POLLHUP))
-
-        cmd = " ".join(args.command)
-        p = Popen(cmd, shell=True, stdin=PIPE, stdout=slave, stderr=slave, close_fds=True)
-        if args.verbose > 1: print "Running: pid:%d, rc:%s" % (p.pid, p.returncode)
-
-        while running:
-            # check we are still running
-            if p.poll() != None:
-                print "child finished: rc = %d" % (p.returncode)
-                running = False
-                return_code = p.returncode
-                break
-
-            # check for IO
-            events = io_poll.poll(1000)
-            #print "events is %s" % (events)
-            for (fd, mask) in events:
-                #print "m_pty fd is %d" % (m_pty.fileno())
-                out = m_pty.read()
-                sys.stdout.write(out)
-
-
-        run_count = run_count + 1
-
-        # Process our exit conditions
-        if args.test == True: complete = True
-        if run_count >= args.limit: complete = True
-        if args.invert:
-            if return_code != 0: complete = True
-        else:
-            if return_code == 0: complete = True
+    for run_count in itertools.count():
+        return_code = subprocess.call(args.command, close_fds=True,
+                                      preexec_fn=become_tty_fg)
+        if args.test == True: break
+        if run_count >= args.limit: break
+        if args.invert and return_code != 0: break
+        elif not args.invert and return_code == 0: break
 
     print "Ran command %d times" % (run_count)
