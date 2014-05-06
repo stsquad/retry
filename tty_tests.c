a0c04f9e9d427c7a180e49cb160d41e601eff570
Alex Bennée <alex.bennee@linaro.org>
2014-05-06 11:21:08 +0100

    tty_tests: expand test to monitor all fds

--- tty_tests.c
+++ tty_tests.c
@@ -11,12 +11,14 @@
 
 int main(int argc, char **argv)
 {
-    int is_tty = isatty(STDOUT_FILENO);
+    int is_tty_stdin = isatty(STDIN_FILENO);
+    int is_tty_stdout = isatty(STDOUT_FILENO);
+    int is_tty_stderr = isatty(STDERR_FILENO);
     int pgrp = getpgrp();
     int tc_pgrp = tcgetpgrp(STDOUT_FILENO);
 
     // This is the initial test scp does
-    fprintf(stderr, "isatty reports %d\n", is_tty);
+    fprintf(stderr, "isatty reports %d/%d/%d\n", is_tty_stdin, is_tty_stdout, is_tty_stderr);
 
     // This test is from can_output() in progressbar.c
     fprintf(stderr, "pgrps are %d and %d\n", pgrp, tc_pgrp);
